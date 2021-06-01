37008e1b57743a5999c17352ffc151ed2cebbbd8
Estefania Vidal Bouzon <estefaniavidalbouzon@gmail.com>
2021-06-01 13:43:11 +0200

    public consultations

--- Pub_cons_api.py
+++ Pub_cons_api.py
@@ -1,12 +1,12 @@
 from os import path
 import requests
 
-PAGES_TO_SCRAPE=2
+PAGES_TO_SCRAPE=9
 
 query = "technology"
 url = "https://ec.europa.eu/info/law/better-regulation/brpapi/searchInitiatives"
 page=0
-size=5
+size=20
 language="EN"
 
 
@@ -14,19 +14,24 @@ def get_consultations(query,page,size,language,url):
     response = requests.get(url, 
         params={'text': query, 'page': page,'size':size,'language':language}
         ).json()
-    content= response["_embedded"]
-    return content
+    try:
+        content= response["_embedded"]
+        return content
+    except:
+        return None
 
 def get_selected_info(content):
+    if not content:
+        return None
     consultations = []
-    for n in range(size):    
-        cons_id = content["initiativeResultDtoes"][n]["id"]
-        title= content["initiativeResultDtoes"][n]["shortTitle"]
-        type_of_act = content["initiativeResultDtoes"][n]["foreseenActType"]
-        topics = content["initiativeResultDtoes"][n]["topics"][0]["label"]
-        status = content["initiativeResultDtoes"][n]["currentStatuses"][0]["receivingFeedbackStatus"]
-        start_date = content["initiativeResultDtoes"][n]["currentStatuses"][0]["feedbackStartDate"]
-        end_date = content["initiativeResultDtoes"][n]["currentStatuses"][0]["feedbackEndDate"]
+    for iniciative in content["initiativeResultDtoes"]:    
+        cons_id = iniciative["id"]
+        title= iniciative["shortTitle"]
+        type_of_act = iniciative["foreseenActType"]
+        topics = iniciative["topics"][0]["label"]
+        status = iniciative["currentStatuses"][0]["receivingFeedbackStatus"]
+        start_date = iniciative["currentStatuses"][0]["feedbackStartDate"]
+        end_date = iniciative["currentStatuses"][0]["feedbackEndDate"]
         title_link= title.replace(" ","-")
         link= f"https://ec.europa.eu/info/law/better-regulation/have-your-say/initiatives/{cons_id}-{title_link}_en"
         consultations.append({'cons_id': cons_id, 'title': title,'topics': type_of_act,"type_of_act":type_of_act,"topics": topics, "status":status, "start_date":start_date,"end_date":end_date,"link":link})
@@ -44,4 +49,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    result = main()
+    print(result)
