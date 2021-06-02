4541a7c6cbcfd940439a3850e452260975520481
Claire Beswick <claire.beswick.h@gmail.com>
2021-06-02 16:28:53 +0200

    website first version

--- app.py
+++ app.py
@@ -4,3 +4,50 @@ import streamlit as st
 
 It's beautiful
 '''
+
+
+# API QUERIES
+query = st.text_input("Search for a topic", 'agriculture')
+
+import requests
+
+
+## TWITTER
+lexai_twitter_url = "http://127.0.0.1:8000/predict"
+tweet_params=dict(keyword=query)
+tweet_likes = requests.get(lexai_twitter_url,params=tweet_params).json()
+
+'''
+## Twitter says
+'''
+st.write('Tweets about this topic were liked by',tweet_likes[0], 'people on Twitter')
+
+## EURLEX
+'''
+## REGULATIONS
+'''
+lexai_eurlex_url = "http://127.0.0.1:8000/query"
+tweet_params=dict(query=query,n=5)
+eurlex = requests.get(lexai_eurlex_url,params=tweet_params).json()
+for i in eurlex:
+    st.write(eurlex[i]['title'])
+    st.write(eurlex[i]['author'])
+    st.write(eurlex[i]['date'])
+    st.write(eurlex[i]['link'])
+
+
+
+## CONSULTS
+'''
+## OPEN CONSULTATIONS
+'''
+lexai_eurlex_url = "http://127.0.0.1:8000/query"
+consults_params=dict(query=query,index='consultations',n=50)
+consults = requests.get(lexai_eurlex_url,params=consults_params).json()
+for i in consults:
+    if consults[i]['status']=='OPEN':
+        st.write('Title: ',consults[i]['title'])
+        st.write('Type of act: ',consults[i]['type_of_act'])
+        st.write('Start date: ',consults[i]['start_date'])
+        st.write('End date: ',consults[i]['end_date'])
+        st.write('Link: ',consults[i]['link'])
