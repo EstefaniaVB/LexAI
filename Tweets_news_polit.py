f5fbeb6985b350e4e216588ccd3251b49f8d47ac
Estefania Vidal Bouzon <estefaniavidalbouzon@gmail.com>
2021-06-03 18:09:40 +0200

    Functions to retrieve Twitter data

--- Tweets_news_polit.py
+++ Tweets_news_polit.py
@@ -55,22 +55,21 @@ politicians_3set = politicians[200:]
 
 #requesting the tweets from the news
 
-def get_politicians_data():
+def get_news_data():
     news_data=[]
-    time.sleep(60*15) #run every 15min
     for outlet in news:
-        news_data.append(python_tweets.get_user_timeline(screen_name=outlet, count=3))
-        
+        try:
+            news_data.append(python_tweets.get_user_timeline(screen_name=outlet, count=3))
+            print(outlet)
+        except:
+            print(outlet + " Not Found")
     news_tweets = pd.DataFrame(news_data)
     return news_tweets
 
-
-#Selecting relevant information
-
 def get_news_data_filtered():
     data_new_filtered=[]
-    for column in get_politicians_data():
-        for row in get_politicians_data()[column]:
+    for column in get_news_data():
+        for row in get_news_data()[column]:
             id_new = row["id"]
             text = row["text"]
             try:
@@ -100,48 +99,26 @@ def get_news_data_filtered():
 
 def get_politicians_data():
     meps_data = []
-    time.sleep(60*15) #seting a sleep time of 15 min
+
     for politic in politicians_1set:
-        print(f"Downloading tweets from: ...{politic}")
-        meps_data.append(python_tweets.get_user_timeline(screen_name=politic, count=2))
+        try:
+            meps_data.append(python_tweets.get_user_timeline(screen_name=politic, count=2))
+            print(f"Downloading tweets from: ...{politic}")
+        except:
+            print(politic + " Not Found")
+
     for politic in politicians_2set:
-        print(f"Downloading tweets from: ...{politic}")
-        meps_data.append(python_tweets.get_user_timeline(screen_name=politic, count=2))
+        try:
+            meps_data.append(python_tweets.get_user_timeline(screen_name=politic, count=2))
+            print(f"Downloading tweets from: ...{politic}")
+        except:
+            print(politic + " Not Found")
+
     for politic in politicians_3set:
-        print(f"Downloading tweets from: ...{politic}")
-        meps_data.append(python_tweets.get_user_timeline(screen_name=politic, count=2))
-        meps_tweets = pd.DataFrame(meps_data)
+        try:
+            meps_data.append(python_tweets.get_user_timeline(screen_name=politic, count=2))
+            print(f"Downloading tweets from: ...{politic}")
+        except:
+            print(politic + " Not Found")
+    meps_tweets = pd.DataFrame(meps_data)
     return meps_tweets
-
-
-def get_politicians_data_filtered():
-    
-    data_politicians_filtered=[]
-
-    for column in get_politicians_data():
-        for row in get_politicians_data()[column]:
-            id_new = row["id"]
-            text = row["text"]
-            try:
-                url = row["entities"]["urls"][0]["url"]
-            except:
-                url =""
-            try:
-                expanded_url = row["entities"]["urls"][0]["expanded_url"]
-            except:
-                expanded_url =""
-            name = row["user"]["name"]
-            location = row["user"]["location"]
-            followers_count = row["user"]["followers_count"]
-            friends_count = row["user"]["friends_count"]
-            listed_count = row["user"]["listed_count"]
-            favourites_count = row["user"]["favourites_count"]
-            profile_image_url = row["user"]["profile_image_url"]
-            retweet_count= row["retweet_count"]
-            source = row["source"]
-            created_at = row["created_at"]
-            
-            data_politicians_filtered.append({"id_new":id_new,"text":text,"url":url,"expanded_url":expanded_url,"name":name,"location":location,"followers_count":followers_count,"friends_count":friends_count,"listed_count":listed_count,"favourites_count":favourites_count,"profile_image_url":profile_image_url,"retweet_count":retweet_count,"source":source,"created_at":created_at})
-    politicians_tweets = pd.DataFrame(data_politicians_filtered)
-    return politicians_tweets
-    
