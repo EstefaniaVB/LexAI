887877c7bd8f45d0f29ac9c838f2ee506d4087dd
Estefania Vidal Bouzon <estefaniavidalbouzon@gmail.com>
2021-06-04 14:35:37 +0200

    Look app

--- app.py
+++ app.py
@@ -1,32 +1,85 @@
 import streamlit as st
+import io
+from typing import List, Optional
+import requests
+import markdown
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.graph_objects as go
+import streamlit as st
+from plotly import express as px
+from plotly.subplots import make_subplots
+import streamlit as st
+import datetime
+import pandas as pd
+from dateutil.relativedelta import relativedelta # to add days or years
 
 st.set_page_config(layout="wide",initial_sidebar_state="expanded")
-c1, c2, c3, c4 = st.beta_columns((2, 1, 1, 1))
 
-'''
-# LexAI
-It's beautiful
-'''
+query = st.text_input("Search for a topic", 'Eg: Technology')
+c1, c2, c3, c4 = st.beta_columns((1, 1, 1, 1))
 
-with st.form("my_form"):
-    st.write("Search by regulation:")
-    checkbox_val = st.checkbox("On Force")
-    checkbox_val = st.checkbox("...")
+lexai_eurlex_url = "http://127.0.0.1:8000/query"
+tweet_params=dict(query=query,n=5)
+eurlex = requests.get(lexai_eurlex_url,params=tweet_params).json()
 
-    # Apply the filters
-    submitted = st.form_submit_button("Apply")
-    if submitted:
-        st.write("slider", "checkbox", checkbox_val)
+def get_regulation():
+    reg = []
+    for i in eurlex:
+        #It would be nice to have a list and small text
+        #reg.append(st.write(eurlex[i]['title']))
+        reg.append(st.write(eurlex[i]['author']))
+        reg.append(st.write(eurlex[i]['date']))
+        reg.append(st.write(eurlex[i]['link']))
+    return reg
 
-st.write("Outside the form")
 
+def get_consultations():
+    lexai_eurlex_url = "http://127.0.0.1:8000/query"
+    consults_params=dict(query=query,index='consultations',n=50)
+    consults = requests.get(lexai_eurlex_url,params=consults_params).json()
+    consultations = []
+    for i in consults:
+        consultations.append(st.write('Title: ',consults[i]['title']))
+        consultations.append(st.write('Type of act: ',consults[i]['type_of_act']))
+        #consultations.append(st.write('Start date: ',consults[i]['start_date']))
+        consultations.append(st.write('End date: ',consults[i]['end_date']))
+        consultations.append(st.write('Link: ',consults[i]['link']))
+    return consultations
 
+#Regulation Box
+
+with c1:
+    
+    '''
+    ## REGULATIONS
+    '''
+    ## Range selector
+    d3 = st.date_input("Filter regulations by date:", [])
+    regulation = get_regulation()
+    
+#Deadlines Box
+with c2:
+    
+    '''
+    ## CONSULTATIONS
+    '''
+    ## Range selector
+    d4 = st.date_input("Filter consultations by date:", [])
+    st.write("Search by type of regulation:")
+    checkbox_val = st.checkbox("Open")
+    checkbox_val = st.checkbox("Closed")
+    expander=st.beta_expander("expand")
+    with expander:
+        consultation = get_consultations()
+
+with c3:
+    '''
+    ## Twitter says nothing
+    '''
 
 # API QUERIES
-query = st.text_input("Search for a topic", 'agriculture')
-
-import requests
-
 #indices=['eurlex', 'consultations', 'twitter_query', 'twitter_press', 'twitter_politicians'])
 
 
@@ -35,39 +88,6 @@ import requests
 #tweet_params=dict(keyword=query)
 #tweet_likes = requests.get(lexai_twitter_url,params=tweet_params).json()
 
-'''
-## Twitter says nothing
-'''
+
 #st.write('Tweets about this topic were liked by',tweet_likes[0], 'people on Twitter')
 
-## Dashboard Style
-
-## EURLEX
-'''
-## REGULATIONS
-'''
-lexai_eurlex_url = "http://127.0.0.1:8000/query"
-tweet_params=dict(query=query,n=5)
-eurlex = requests.get(lexai_eurlex_url,params=tweet_params).json()
-for i in eurlex:
-    st.write(eurlex[i]['title'])
-    st.write(eurlex[i]['author'])
-    st.write(eurlex[i]['date'])
-    st.write(eurlex[i]['link'])
-
-
-
-## CONSULTS
-'''
-## OPEN CONSULTATIONS
-'''
-lexai_eurlex_url = "http://127.0.0.1:8000/query"
-consults_params=dict(query=query,index='consultations',n=50)
-consults = requests.get(lexai_eurlex_url,params=consults_params).json()
-for i in consults:
-    if consults[i]['status']=='OPEN':
-        st.write('Title: ',consults[i]['title'])
-        st.write('Type of act: ',consults[i]['type_of_act'])
-        st.write('Start date: ',consults[i]['start_date'])
-        st.write('End date: ',consults[i]['end_date'])
-        st.write('Link: ',consults[i]['link'])
