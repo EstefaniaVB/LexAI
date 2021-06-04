ca80089c96885e11cce8d3f4834ad3772abc34f5
Estefania Vidal Bouzon <estefaniavidalbouzon@gmail.com>
2021-06-04 18:21:11 +0200

    notebook streamlit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import io
 from typing import List, Optional
+import streamlit.components.v1 as components
 import requests
 import markdown
 import matplotlib
@@ -14,11 +15,17 @@ import streamlit as st
 import datetime
 import pandas as pd
 from dateutil.relativedelta import relativedelta # to add days or years
+from IPython.core.display import display, HTML
+
 
 st.set_page_config(layout="wide",initial_sidebar_state="expanded")
+'''
+    # LexAI
+    ## Navigating public fora
+    '''
 
-query = st.text_input("Search for a topic", 'Eg: Technology')
-c1, c2, c3, c4 = st.beta_columns((1, 1, 1, 1))
+query = st.text_input("Search for a topic", 'Technology')
+c1, c2, c3 = st.beta_columns((1, 1, 2))
 
 lexai_eurlex_url = "http://127.0.0.1:8000/query"
 tweet_params=dict(query=query,n=5)
@@ -48,12 +55,39 @@ def get_consultations():
         consultations.append(st.write('Link: ',consults[i]['link']))
     return consultations
 
+def get_news():
+    consult_params=dict(query=query,index='twitter_press',n=10)
+    press = requests.get(lexai_eurlex_url,params=consult_params).json()
+    html_code =[]
+    for i in press:
+        link = press[i]["link"]
+        text= press[i]["text"]
+        user= press[i]["user"]
+        date= press[i]["date"]
+        html_link = f'<blockquote class="twitter-tweet"><p lang="en" dir="ltr">{text}.<a href={link}</a></p>&mdash; {user} (@{user}) <a href={link}>{date}</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
+        html_code.append(html_link)
+    return html_code
+
+def get_news2():
+    consult_params=dict(query=query,index='twitter_press',n=50)
+    press = requests.get(lexai_eurlex_url,params=consult_params).json()
+    info=[]
+    for i in press:
+        link = press[i]["link"]
+        text= press[i]["text"]
+        user= press[i]["user"]
+        date= press[i]["date"]
+        #html_link = f'<blockquote class="twitter-tweet"><p lang="en" dir="ltr">{text}.<a href={link}</a></p>&mdash; {user} (@{user}) <a href={link}>{date}</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
+        html_link = f'<blockquote data-cards="hidden" class="twitter-tweet" data-height="1%" data-width="100%"> <p lang="en" dir="ltr">{text}.<a href={link}</a></p>&mdash; {user} (@{user}) <a href={link}>{date}</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
+        info.append({"link":link,"text":text,"user":user,"date":date,"html_link":html_link})
+    return pd.DataFrame(info).sort_values(by="date", ascending=False)
+
 #Regulation Box
 
 with c1:
     
     '''
-    ## REGULATIONS
+    ## Regulations
     '''
     ## Range selector
     d3 = st.date_input("Filter regulations by date:", [])
@@ -63,7 +97,7 @@ with c1:
 with c2:
     
     '''
-    ## CONSULTATIONS
+    ## Consultations
     '''
     ## Range selector
     d4 = st.date_input("Filter consultations by date:", [])
@@ -74,19 +108,29 @@ with c2:
     with expander:
         consultation = get_consultations()
 
+
+## Displaying tweets (WE NEED TO SORT THE TWEETS BY LASTEST PUBLISHED)
 with c3:
     '''
-    ## Twitter says nothing
+    ## Industry News
     '''
+    expander=st.beta_expander("expand")
+    with expander:
+        html_tweet = get_news2()["html_link"]
+        list_of_tweets = []
+        for e in range(len(html_tweet)):
+            list_of_tweets.append(components.html(html_tweet[e],scrolling=True,width=500,height=200))
+    
+
 
 # API QUERIES
 #indices=['eurlex', 'consultations', 'twitter_query', 'twitter_press', 'twitter_politicians'])
 
 
 ## TWITTER
-#lexai_twitter_url = "http://127.0.0.1:8000/predict"
-#tweet_params=dict(keyword=query)
-#tweet_likes = requests.get(lexai_twitter_url,params=tweet_params).json()
+lexai_twitter_url = "http://127.0.0.1:8000/"
+tweet_params=dict(keyword=query)
+tweet_likes = requests.get(lexai_twitter_url,params=tweet_params).json()
 
 
 #st.write('Tweets about this topic were liked by',tweet_likes[0], 'people on Twitter')
