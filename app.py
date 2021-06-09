7d94b6e0aba7f91098110010c7e205a42d85fc5a
Claire Beswick <claire.beswick.h@gmail.com>
2021-06-09 14:58:19 +0200

    '

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 app = MultiApp()
 
 # Add all your application here
-app.add_app("Data", data.app)
-app.add_app("Model", model.app)
+app.add_app("Regulations", data.app)
+app.add_app("Opinions", model.app)
 # The main app
 app.run()
